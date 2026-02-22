c386ea8d8a6905d2f3ca763fe1543a37a3642346
naaziim <naaziim@gmail.com>
2026-02-22 20:01:58 +0100

    adjust

--- EKS-TF/iam-instance-profile.tf
+++
@@ -1,8 +0,0 @@
-data "aws_iam_role" "lab_role" {
-  name = "LabRole"
-}
-
-resource "aws_iam_instance_profile" "instance-profile" {
-  name = "Jenkins-instance-profile"
-  role = data.aws_iam_role.lab_role.name
-}
