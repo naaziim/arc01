a42290a488f0dd7640a5d59d47da558538c77174
naaziim <naaziim@gmail.com>
2026-02-22 20:02:56 +0100

    adjust

--- EKS-TF/eks-node-group.tf
+++ EKS-TF/eks-node-group.tf
@@ -14,9 +14,4 @@ resource "aws_eks_node_group" "eks-node-group" {
   instance_types = ["t3a.medium"]
   disk_size      = 20
 
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
-  ]
 }
